bf7ac3ea81d22eeca60a07d17305ebc38d9538c3
akaganov123 <119634271+akaganov123@users.noreply.github.com>
2022-12-05 14:51:53 -0800

    5902052580562

--- GameDesign2_Game2/Assets/Scripts/EventBase.cs
+++ GameDesign2_Game2/Assets/Scripts/EventBase.cs
@@ -6,6 +6,9 @@ public class EventBase : MonoBehaviour
 {
 Ray ray;
 RaycastHit hitData;
+float hitDistance;
+Vector3 hitPosition;
+string tag;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +18,12 @@ RaycastHit hitData;
     // Update is called once per frame
     void Update()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3 (0.5f, 0.5f, 0));
-        Vector3 hitPosition = hitData.point;
-        float hitDistance = hitData.distance;
-        string tag = hitData.collider.tag;
+        if(Physics.Raycast(ray,out hitData)){
 
-
-
-        
+        }
+        ray = Camera.main.ViewportPointToRay(new Vector3 (0.5f, 0.5f, 0));
+        hitPosition = hitData.point;
+        hitDistance = hitData.distance;
+        Debug.Log(hitData.collider.gameObject.tag);
     }
 }
